456b752bd336a4d515975712ee1335f9442a6c38
Abhinav Awana <114151684+ZcRaaTch@users.noreply.github.com>
2025-01-13 14:31:22 +0530

    Time: 11 ms (77.02%), Space: 31.9 MB (93.55%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        int n= s.length();
-        if(n<3) return n;
-        int ans=0;
-        map<char,int> mp;
-        for(auto c:s){
-            if(mp.find(c)==mp.end()){
-                mp[c]=1;
-            }else{mp[c]++;}
-            if(mp[c]>=3) mp[c]-=2;
-        }
-        for(auto m:mp) {
-            ans+=m.second;
-        }
-        return ans;
+    ios::sync_with_stdio(false);
+    vector<int>ct(26);
+    for(char c:s) ct[c-'a']++;
+    int res = 0;
+    for(int i=0;i<26;i++){if (ct[i] % 2 != 0) {
+    res += 1;  // If ct[i] is odd
+    } else if (ct[i] != 0) {
+        res += 2;  // If ct[i] is even and non-zero
+    } else {
+        res += 0;  // If ct[i] is zero
+    }}
+    return res;
     }
 };
