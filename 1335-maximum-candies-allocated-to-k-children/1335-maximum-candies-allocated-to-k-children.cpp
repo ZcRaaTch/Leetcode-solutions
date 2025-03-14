2d04bbf35cd32d687034eb773e47a034297ac96f
Abhinav Awana <114151684+ZcRaaTch@users.noreply.github.com>
2025-03-14 18:03:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        int low=0,high=1e7;
+        while(low<high){
+            long sum=0,mid=(low+high+1)/2;
+            for(auto& c:candies){
+                sum+=c/mid;
+            }
+            if(k>sum) high=mid-1;
+            else low=mid;
+        }
+        return left;
+    }
+};
